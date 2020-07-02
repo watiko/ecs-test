92175947733097815a433f4083c875f5908496fd
watiko <service@mail.watiko.net>
2020-07-02 18:02:44 +0900

    feat(tester): improved

--- tester/test.js
+++ tester/test.js
@@ -1,18 +1,41 @@
 const got = require('got');
+const { Agent } = require('http');
 
 const PORT = process.env.PORT || 8080;
 
-async function wait(sec) {
+const sleep = ms => new Promise(resolve => {
+  setTimeout(resolve, ms);
+})
+
+const jitteredSleep = maxSec => sleep(Math.random() * maxSec * 1000);
+
+async function reqWait(sleepSec, waitSec) {
+  const options = {
+    agent: {
+      http: new Agent({ keepAlive: true }),
+    },
+  };
+
   while (true) {
-    const res = await got(`http://localhost:${PORT}/wait/${sec}`);
-    console.log(res.body);
+    await jitteredSleep(10);
+    try {
+      const res = await got(`http://localhost:${PORT}/wait/${waitSec}`, options);
+      console.log(res.body);
+      await sleep(sleepSec * 1000);
+    } catch (err) {
+      console.error('failed to request', err);
+    }
   }
 }
 
 async function main() {
-  const waits = [5, 15, 30, 60, 60 * 2, 60 * 3];
+  let params = [[20, 30]];
+
+  for (let i = 0; i < 100; i++) {
+    params = [[1, 1], [2, 0.45], [3, 0.45], [4, 0.45], ...params]
+  }
   
-  await Promise.all(waits.map(wait));
+  await Promise.all(params.map(([sleepSec, waitSec]) => reqWait(sleepSec, waitSec)));
 }
 
 main()
